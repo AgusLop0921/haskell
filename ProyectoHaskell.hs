1cdb1aca3ed68f255ff06c87fe27a159575cccf7
Sergio Agustin Lopez <aguslop0921@gmail.com>
2020-10-11 15:22:27 -0300

    Update ProyectoHaskell.hs

--- ProyectoHaskell.hs
+++ ProyectoHaskell.hs
@@ -1,76 +1,84 @@
 import qualified Data.HashMap.Strict as H
 
-type TMSymTable = (H.HashMap String TMInstruction)
+type TMSymTable = (H.HashMap String TMInstruccion)
 
-data TMState = Estado TMSymTable
+data TMEstado = Estado TMSymTable
   deriving (Show, Eq)
 
-data TMInstruction = Instruccion String Direction String
+data TMInstruccion = Instruccion String Direccion String
   deriving (Show, Eq)
 
-data Direction = CintaDerecha
+data Direccion = CintaDerecha
                | CintaIzquierda
   deriving (Show, Eq)
 
-type Maquina = (H.HashMap String TMState)
+type Maquina = (H.HashMap String TMEstado)
 type Cinta = [String]
 type Alfabeto = [String]
 
 ejecutarPaso :: Maquina -> Alfabeto -> Cinta -> String -> Int -> (Cinta, String, Int)
-ejecutarPaso maquina alfabeto cinta estadoInicial position = procesarEstado (H.lookup estadoInicial maquina) cinta position
+ejecutarPaso maquina alfabeto cinta estadoInicial posicion = procesarEstado (H.lookup estadoInicial maquina) cinta posicion
 
+procesarEstado :: Maybe TMEstado -> Cinta -> Int -> (Cinta, String, Int)
+procesarEstado (Just (Estado symTable)) cinta posicion = handleAddingTape (procesarInstruccion (H.lookup (cinta!!posicion) symTable) cinta posicion)
 
-procesarEstado (Just (Estado symTable)) cinta position = handleAddingTape (procesarInstruccion (H.lookup (cinta!!position) symTable) cinta position)
+procesarInstruccion :: Maybe TMInstruccion -> Cinta -> Int -> ([String], String,Int)
+procesarInstruccion (Just (Instruccion escribirSimbolo dir proximoEstado)) cinta posicion
+    | dir == CintaDerecha = (remplazarEnIndice posicion escribirSimbolo cinta, proximoEstado, (posicion-1))
+    | otherwise = (remplazarEnIndice posicion escribirSimbolo cinta, proximoEstado, (posicion+1))
 
-procesarInstruccion (Just (Instruccion writeSymbol dir nextState)) cinta position
-    | dir == CintaDerecha = (remplazarEnIndice position writeSymbol cinta, nextState, (position-1))
-    | otherwise = (remplazarEnIndice position writeSymbol cinta, nextState, (position+1))
-
-handleAddingTape (cinta, nextState, position)
-    | position < 0 = ("":cinta, nextState, position+1)
-    | position == (length cinta) = (cinta ++ [""], nextState, position)
-    | otherwise = (cinta, nextState, position)
+handleAddingTape (cinta, proximoEstado, posicion)
+    | posicion < 0 = ("":cinta, proximoEstado, posicion+1)
+    | posicion == (length cinta) = (cinta ++ [""], proximoEstado, posicion)
+    | otherwise = (cinta, proximoEstado, posicion)
 
 runVerbose :: Maquina -> Alfabeto -> Cinta -> String -> IO ()
 runVerbose maquina alfabeto cinta estadoInicial = loop 0 maquina alfabeto cinta estadoInicial 0
 
 loop :: Int -> Maquina -> Alfabeto -> Cinta -> String -> Int -> IO ()
-loop i maquina alfabeto cinta estadoInicial position =
-                           do  putStr ("Estado " ++ show i ++ ". Cinta: [" ++ showTape cinta position "" 0 ++ "]" ++ "\n" ++ "Estado " ++ show i ++ ". En estado " ++ show estadoInicial ++ "\n"
+loop i maquina alfabeto cinta estadoInicial posicion =
+                           do  putStr ("Estado " ++ show i ++ ". Cinta: [" ++ mostrarCinta cinta posicion "" 0 ++ "]" ++ "\n" ++ "Estado " ++ show i ++ ". En estado " ++ show estadoInicial ++ "\n"
                                         -- ++ ": [" ++ showState alfabeto (H.lookup estadoInicial maquina) ++ "]\n"
-                                        ++ "Estado " ++ show i ++ ". " ++ showInstruction maquina alfabeto cinta estadoInicial position ++ "\n\n")
-                               let (newTape, newState, newPosition) = ejecutarPaso maquina alfabeto cinta estadoInicial position in
-                                   if newState == "HALT"
-                                        then putStr ("Estado " ++ show (i+1) ++ ". Cinta: [" ++ showTape newTape newPosition "" 0 ++ "]" ++ "\n" ++ "Estado " ++ show (i+1) ++ ". DETENER Programa Completo." ++ "\n")
-                                   else loop (i+1) maquina alfabeto newTape newState newPosition
+                                        ++ "Estado " ++ show i ++ ". " ++ showInstruction maquina alfabeto cinta estadoInicial posicion ++ "\n\n")
+                               let (nuevaCinta, nuevoEstado, newPosition) = ejecutarPaso maquina alfabeto cinta estadoInicial posicion in
+                                   if nuevoEstado == "HALT"
+                                        then putStr ("Estado " ++ show (i+1) ++ ". Cinta: [" ++ mostrarCinta nuevaCinta newPosition "" 0 ++ "]" ++ "\n" ++ "Estado " ++ show (i+1) ++ ". DETENER Programa Completo." ++ "\n")
+                                   else loop (i+1) maquina alfabeto nuevaCinta nuevoEstado newPosition
 
 
 
-showTape :: Cinta -> Int -> String -> Int -> String
-showTape [] position str i = str
-showTape (a:cinta) position str i
-    | position == i = showTape cinta position (str ++ ((getPrefix i) ++ "*" ++ show a)) (i+1)
-    | otherwise = showTape cinta position (str ++ ((getPrefix i) ++ show a)) (i+1)
+mostrarCinta :: Cinta -> Int -> String -> Int -> String
+mostrarCinta [] posicion str i = str
+mostrarCinta (a:cinta) posicion str i
+    | posicion == i = mostrarCinta cinta posicion (str ++ ((obtenerPrefijo i) ++ "*" ++ show a)) (i+1)
+    | otherwise = mostrarCinta cinta posicion (str ++ ((obtenerPrefijo i) ++ show a)) (i+1)
 
-getPrefix :: Int -> String
-getPrefix i
+obtenerPrefijo :: Int -> String
+obtenerPrefijo i
     | i > 0 = ", "
     | otherwise = ""
 
-addToPrintStr :: Maybe TMInstruction -> String -> String -> String
-addToPrintStr (Just (Instruccion symbol dir nextState)) readSym str = (str ++ "(read " ++ show readSym ++ ", write " ++ show symbol ++ ", movimiento " ++ show dir ++ ", proximo estado: " ++ show nextState ++ ") ")
+addToPrintStr :: Maybe TMInstruccion -> String -> String -> String
+addToPrintStr (Just (Instruccion simbolo dir proximoEstado)) readSym str = (str ++ "(read " ++ show readSym ++ ", write " ++ show simbolo ++ ", movimiento " ++ show dir ++ ", proximo estado: " ++ show proximoEstado ++ ") ")
 addToPrintStr Nothing readSym str = str
 
-showInstruction maquina alfabeto cinta estadoInicial position = showInstructionHelper (H.lookup estadoInicial maquina) cinta position
+showInstruction maquina alfabeto cinta estadoInicial posicion = showInstructionHelper (H.lookup estadoInicial maquina) cinta posicion
 
-showInstructionHelper (Just (Estado symTable)) cinta position = (showInstructionHelper2 (H.lookup (cinta!!position) symTable) (cinta!!position))
+showInstructionHelper (Just (Estado symTable)) cinta posicion = (showInstructionHelper2 (H.lookup (cinta!!posicion) symTable) (cinta!!posicion))
 
-showInstructionHelper2 (Just (Instruccion writeSymbol dir nextState)) readSymbol = ("Instruccion: ( Leyendo  " ++ show readSymbol ++ ", write " ++ show writeSymbol ++ ", movimiento " ++ show dir ++ ", proximo estado: " ++ show nextState ++ ")")
+showInstructionHelper2 (Just (Instruccion escribirSimbolo dir proximoEstado)) readSymbol = ("Instruccion: ( Leyendo  " ++ show readSymbol ++ ", write " ++ show escribirSimbolo ++ ", movimiento " ++ show dir ++ ", proximo estado: " ++ show proximoEstado ++ ")")
 
 
 -- From: http://stackoverflow.com/questions/10133361/haskell-replace-element-in-list
 remplazarEnIndice :: Int -> String -> [String] -> [String]
 remplazarEnIndice n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls
+
+
+alfabeto = ["", "0", "1"]
+cinta = ["1", "1", "1"]
+maquina = H.fromList [
+                        ("0",Estado (H.fromList [("",Instruccion "" CintaDerecha "1"), ("0",Instruccion "0" CintaIzquierda "0"), ("1",Instruccion "1" CintaIzquierda "0")])),("1",Estado (H.fromList [("",Instruccion "1" CintaIzquierda "2"), ("0",Instruccion "1" CintaDerecha "2"), ("1",Instruccion "0" CintaDerecha "1")])),("2",Estado (H.fromList [("",Instruccion "" CintaDerecha "HALT"), ("0",Instruccion "0" CintaIzquierda "2"), ("1",Instruccion "1" CintaIzquierda "2")]))]
+
 
 
 alfabeto = ["", "0", "1"]
