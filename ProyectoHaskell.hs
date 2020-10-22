a7c962969353546755ea8fd73895aac4371e3bc8
faimaro <aimarofran@gmail.com-at-456970202383>
2020-10-21 23:35:42 -0300

    mejoras en codigo

--- ProyectoHaskell.hs
+++ ProyectoHaskell.hs
@@ -13,27 +13,28 @@ data Direccion = CintaDerecha
   deriving (Show, Eq)
 
 type Maquina = (H.HashMap String TMEstado)
-type Cinta = [String]
+type Cinta = [String] --tambien se puede usar seq . para que sea mas facil escribirla
+
 type Alfabeto = [String]
 
 ejecutarPaso :: Maquina -> Alfabeto -> Cinta -> String -> Int -> (Cinta, String, Int)
 ejecutarPaso maquina alfabeto cinta estadoInicial posicion = procesarEstado (H.lookup estadoInicial maquina) cinta posicion
 
 procesarEstado :: Maybe TMEstado -> Cinta -> Int -> (Cinta, String, Int)
-procesarEstado (Just (Estado symTable)) cinta posicion = adicionACinta (procesarInstruccion (H.lookup (cinta!!posicion) symTable) cinta posicion)
+procesarEstado (Just (Estado symTable)) cinta posicion = aumentarCinta (procesarInstruccion (H.lookup (cinta!!posicion) symTable) cinta posicion)
 
-procesarInstruccion :: Maybe TMInstruccion -> Cinta -> Int -> ([String], String,Int)
+procesarInstruccion :: Maybe TMInstruccion -> Cinta -> Int -> (Cinta, String,Int)
 procesarInstruccion (Just (Instruccion escribirSimbolo dir proximoEstado)) cinta posicion
     | dir == CintaDerecha = (remplazarEnIndice posicion escribirSimbolo cinta, proximoEstado, (posicion-1))
     | otherwise = (remplazarEnIndice posicion escribirSimbolo cinta, proximoEstado, (posicion+1))
 
-adicionACinta (cinta, proximoEstado, posicion)
-    | posicion < 0 = ("":cinta, proximoEstado, posicion+1)
+aumentarCinta (cinta, proximoEstado, posicion)
+    | posicion < 0 = ([""] ++ cinta, proximoEstado, posicion+1)
     | posicion == (length cinta) = (cinta ++ [""], proximoEstado, posicion)
     | otherwise = (cinta, proximoEstado, posicion)
 
-ejecutar :: Maquina -> Alfabeto -> Cinta -> String -> Int -> IO ()
-ejecutar maquina alfabeto cinta estadoInicial paso = bucle 0 paso maquina alfabeto cinta estadoInicial 0
+ejecutar :: Int -> Maquina -> Alfabeto -> Cinta -> String -> IO ()
+ejecutar paso maquina alfabeto cinta estadoInicial = bucle 0 paso maquina alfabeto cinta estadoInicial 0
 
 bucle :: Int -> Int ->  Maquina -> Alfabeto -> Cinta -> String -> Int -> IO ()
 bucle i paso maquina alfabeto cinta estadoInicial posicion =
@@ -48,9 +49,24 @@ bucle i paso maquina alfabeto cinta estadoInicial posicion =
 
 
 -- From: http://stackoverflow.com/questions/10133361/haskell-replace-element-in-list
-remplazarEnIndice :: Int -> String -> [String] -> [String]
+remplazarEnIndice :: Int -> String -> Cinta -> Cinta
 remplazarEnIndice n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls
 
 alfabeto = ["", "0", "1"]
 cinta = ["1", "1", "1"]
-maquina = H.fromList [ ("0",Estado (H.fromList [("",Instruccion "" CintaDerecha "1"), ("0",Instruccion "0" CintaIzquierda "0"), ("1",Instruccion "1" CintaIzquierda "0")])),("1",Estado (H.fromList [("",Instruccion "1" CintaIzquierda "2"), ("0",Instruccion "1" CintaDerecha "2"), ("1",Instruccion "0" CintaDerecha "1")])),("2",Estado (H.fromList [("",Instruccion "" CintaDerecha "FIN"), ("0",Instruccion "0" CintaIzquierda "2"), ("1",Instruccion "1" CintaIzquierda "2")]))]
+maquina = H.fromList [ 
+  ("0",Estado (H.fromList [
+    ("",Instruccion "" CintaDerecha "1"),
+    ("0",Instruccion "0" CintaIzquierda "0"),
+    ("1",Instruccion "1" CintaIzquierda "0")])),
+
+  ("1",Estado (H.fromList [
+    ("",Instruccion "1" CintaIzquierda "2"), 
+    ("0",Instruccion "1" CintaDerecha "2"), 
+    ("1",Instruccion "0" CintaDerecha "1")])),
+
+  ("2",Estado (H.fromList [
+    ("",Instruccion "" CintaDerecha "FIN"), 
+    ("0",Instruccion "0" CintaIzquierda "2"), 
+    ("1",Instruccion "1" CintaIzquierda "2")]))
+    ]
